324e6ee55b15047b3f5e250197b29e11a8cee87c
Tim Officer <timothyofficer@gmail.com>
2018-09-19 10:35:03 -0400

    return a PanelResult object

--- src/service.js
+++ src/service.js
@@ -12,21 +12,67 @@ function generateGuid() {
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
-function show(options) {
+class PanelResult {
+  constructor(id, promise, panelOptions) {
+    if (!id) throw new Error('id');
+    if (!promise) throw new Error('promise');
+    if (!panelOptions) throw new Error('panelOptions');
+
+    this._id = id;
+    this._promise = promise;
+    this._panelOptions = panelOptions;
+  }
+
+  get promise() {
+    return this._promise;
+  }
+
+  /**
+   * @param {Object} [updatedOptions]
+   * @param {Object} [updatedOptions.props] - any props you want to update
+   */
+  show(updatedOptions = {}) {
+    const panelOptions = Object.assign(this._panelOptions, updatedOptions);
+
+    return showExistingPanelById(this._id, panelOptions);
+  }
+
+  hide() {
+    const panelOptions = Object.assign({
+      id: this._id
+    }, this._panelOptions);
+
+    eventBus.$emit('hideSlideOutPanel', panelOptions);
+  }
+}
+
+function showNewPanel(options) {
+  return showPanel(options);
+}
+
+function showExistingPanelById(id, options) {
+  return showPanel(options, id);
+}
+
+function showPanel(options, existingId) {
   if (!options) throw new Error('options is required');
   if (!options.component) throw new Error('options.component is required');
 
-  options.id = generateGuid();
+  const id = existingId || generateGuid();
 
-  return new Promise(resolve => {
+  options.id = id;
+
+  const promise = new Promise(resolve => {
     eventBus.$emit('showSlideOutPanel', options);
 
     eventBus.$once(`hideSlideOutPanel-${options.id}`, payload => {
       return resolve(payload.data);
     });
   });
+
+  return new PanelResult(id, promise, options);
 }
 
 export default {
-  show
+  show: showNewPanel
 };
