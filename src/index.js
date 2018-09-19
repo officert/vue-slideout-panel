dcdcc7ff87ba331cc40c0c05d3fc04b8d7265a43
Tim Officer <timothyofficer@gmail.com>
2018-09-19 19:55:38 -0400

    use global vue

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import Vue from 'vue';
-
 import VueSlideoutPanel from './components/SlideoutPanel';
 import vueSlideoutPanelService from './service';
 
 // expose component and service to global scope
 if (typeof window !== 'undefined' && window.Vue) {
-  console.log('HEREEE');
-
-  Vue.component('slideout-panel', VueSlideoutPanel);
-  Vue.use({
-    install(RealVue) {
-      RealVue.prototype.$showPanel = vueSlideoutPanelService.show;
+  window.Vue.component('slideout-panel', VueSlideoutPanel);
+  window.Vue.use({
+    install(Vue) {
+      Vue.prototype.$showPanel = vueSlideoutPanelService.show;
     }
   });
 
