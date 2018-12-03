2c7ce05f700f859b1a5de9fdf649abeff63e3957
Tim Officer <timothyofficer@gmail.com>
2018-12-03 09:47:11 -0500

    favicon

--- docs-src/index.js
+++ docs-src/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
-import {
-  VueSlideoutPanel
-} from '../src/index';
+import '../src/index';
 
 import App from './components/App';
 import Panel1 from './components/Panel1';
@@ -11,7 +9,6 @@ import router from './router';
 
 window.vue2PanelDebug = true;
 
-Vue.component('slideout-panel', VueSlideoutPanel);
 Vue.component('panel-1', Panel1);
 Vue.component('panel-2', Panel2);
 Vue.component('panel-3', Panel3);
